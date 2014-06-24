b848809cce72562d989d26ccee17c36782b5d75a
crcanfly11 <crcanfly11@users.noreply.github.com>
2014-06-24 23:59:49 +0800

    pushed

--- boostserver/CBstServer.h
+++ boostserver/CBstServer.h
@@ -9,10 +9,10 @@
 #include <boost/shared_ptr.hpp>
 
 #include <ostream>
-//asdfjcr
+
 using namespace std;
 using namespace boost::asio;
-//asdfjcr
+
 class CBstServer
 {
 	typedef boost::shared_ptr<ip::tcp::socket> socket_type;
